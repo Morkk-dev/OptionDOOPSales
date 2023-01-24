72f73e33f3b38679fa21ed159b0ec53d7fc3beea
lholmer <52855693+lholmer24@users.noreply.github.com>
2023-01-24 12:41:46 +0100

    Initial commit
    I am the developer now

---
+++ Sales.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Sales {
+
+    private String itemId;
+    private double value;
+    private int quantity;
+
+    public Sales(String itemId, double value, int quantity){
+        this.itemId = itemId;
+        this.value = value;
+        this.quantity = quantity;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
